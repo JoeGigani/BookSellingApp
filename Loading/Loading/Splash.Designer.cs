51b6f328a50eb3538e76d82fc1e0ecf92e1b9ecd
გიგა გიგანი <g.gigani@itstep.ge>
2023-05-25 03:13:47 +0400

    designed loading page

--- Loading/Loading/Splash.Designer.cs
+++ Loading/Loading/Splash.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Loading
 {
-    partial class Form1
+    partial class Splash
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
